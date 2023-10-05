97234d723e09cdf4dca4cca1fcbd9512fadff9ba
Nelson Neto <nelson16.code@gmail.com>
2023-10-05 05:08:04 -0400

    feat: add prisma

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-    res.json("wellcome to api");
+  res.json("wellcome to api");
+});
+
+app.get('/prisma', async (req, res) => {
+  const result : any[] = await prisma.$queryRaw`SELECT * FROM restaurants`;
+
+  const data : string[] = [];
+
+  result.forEach(element => {
+    data.push(element.name);
+  });
+
+  res.json(data);
 });
 
 const server = app.listen(3000, () =>
