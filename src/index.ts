43ca43549c55c55e70d08fbf26e96fe05bd3775c
Nelson Neto <nelson16.code@gmail.com>
2023-10-15 23:50:26 -0400

    feat: add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,53 @@
 import express from 'express';
 import http from 'http';
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
 import { PrismaClient } from '@prisma/client';
+
+const httpPort = 8080;
+const wsPort = 3000;
+
 const prisma = new PrismaClient();
 
 const app = express();
 
 const httpServer = http.createServer(app);
+
 const io = new Server(httpServer);
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  res.json("wellcome to api");
+    res.json('wellcome to api');
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+    console.log('a user connected');
 
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 
-  socket.on('message', (msg) => {
-    console.log('message: ' + msg);
-    socket.broadcast.emit("message", 'message from client: ' + msg);
-  });
+    socket.on('message', (msg) => {
+        console.log('message: ' + msg);
+        socket.broadcast.emit('message', 'message from client: ' + msg);
+    });
 });
 
 app.get('/prisma', async (req, res) => {
-  const result: any[] = await prisma.$queryRaw`SELECT * FROM restaurants`;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const result: any[] = await prisma.$queryRaw`SELECT * FROM restaurants`;
 
-  const data: string[] = [];
+    const data: string[] = [];
 
-  result.forEach(element => {
-    data.push(element.name);
-  });
+    result.forEach(element => {
+        data.push(element.name);
+    });
 
-  res.json(data);
+    res.json(data);
 });
 
-app.listen(8080, () =>
-  console.log(`🚀 HTTP Server ready at: http://localhost:8080`),
+app.listen(httpPort, () =>
+    console.log(`🚀 HTTP Server ready at: http://localhost:${httpPort}`),
 );
 
-io.listen(3000);
+io.listen(wsPort);
